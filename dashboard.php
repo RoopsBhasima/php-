8924986a42f20ce99afd7b4a03841244bf70800e
RoopsBhasima <45381034+RoopsBhasima@users.noreply.github.com>
2018-12-17 09:21:34 +0545

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<?php
+include('inc_session.php');
+?>
+Hello 
+<?php echo $_SESSION['username'];?>
+
+<a href="logout.php">Logout</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+
