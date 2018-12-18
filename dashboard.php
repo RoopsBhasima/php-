02558ae85cb3393a13cfb4936fa38e73ee667661
RoopsBhasima <45381034+RoopsBhasima@users.noreply.github.com>
2018-12-18 10:38:24 +0545

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -5,15 +5,4 @@ Hello
 <?php echo $_SESSION['username'];?>
 
 <a href="logout.php">Logout</a>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
-
+<a href="allusers.php">view_users</a>
